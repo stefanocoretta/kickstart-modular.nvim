a89709b8a3684fcd798dfd55f0896741806f1854
stefanocoretta <stefano.coretta@gmail.com>
2024-04-10 11:45:29 +0100

    don't write to disk for otter

--- lua/custom/plugins/otter.lua
+++ lua/custom/plugins/otter.lua
@@ -1,9 +1,15 @@
+-- create .lintr in your $HOME with the following (fixes object length/name error)
+-- linters: all_linters(
+--    object_length_linter = NULL,
+--    object_name_linter = NULL
+--  )
+
 return {
   -- for lsp features in code cells / embedded code
   'jmbuhr/otter.nvim',
   opts = {
     buffers = {
-      write_to_disk = true,
+      write_to_disk = false,
     },
   },
 }
